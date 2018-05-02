62d1571298a1ab8eed6541a603aa9f41a2275a80
maxian <Ma@danglaoshi.net>
2018-05-02 13:32:39 +0800

    删除异常

--- src/main/java/top/onceio/core/beans/BeansEden.java
+++ src/main/java/top/onceio/core/beans/BeansEden.java
@@ -3,7 +3,6 @@ package top.onceio.core.beans;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -434,7 +433,7 @@ public class BeansEden {
 
 	private JsonConfLoader conf = null;
 
-	public void resovle(String... packages) throws SQLException {
+	public void resovle(String... packages) {
 		conf = JsonConfLoader.loadConf("conf");
 		
 		scanner.scanPackages(packages);
